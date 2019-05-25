16dcd8bc6da03be9f3ac74dc25916867960afffb
Relax <Loginovskikh.K@gmail.com>
2019-05-25 20:31:14 +0300

    create redux working desc

---
+++ src/model/data/initialState.js
@@ -0,0 +1,17 @@
+export const data = {
+	groups: {
+		group1: {
+			words: [
+				"word1",
+				"word2"
+			]
+		},
+		group2: {
+			words: [
+				"word3",
+				"word4"
+			]
+		}
+	},
+	mix: ""
+}
---
+++ src/model/reducer.js
@@ -0,0 +1,15 @@
+import {data} from "./data/initialState.js"
+
+export const groups = (state = data.groups, action) => {
+	switch (action.type) {
+		default:
+			return state
+	}
+}
+
+export const mix = (state = data.mix, action) => {
+	switch (action.type) {
+		default:
+			return state
+	}
+}
