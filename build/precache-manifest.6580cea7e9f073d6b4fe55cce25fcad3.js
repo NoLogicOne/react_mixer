86f1a68410cdead51a2b05fffa5835656e72e992
Relax <Loginovskikh.K@gmail.com>
2019-06-09 17:16:49 +0300

    MIXER v 1.0

---
+++ build/precache-manifest.6580cea7e9f073d6b4fe55cce25fcad3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6f7bb1b9c26e265da6083fb353b972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b241ab0e18eb399d55be",
+    "url": "/static/css/main.c25f58e9.chunk.css"
+  },
+  {
+    "revision": "a059fe83d862b3cf1e53",
+    "url": "/static/js/2.01358d8b.chunk.js"
+  },
+  {
+    "revision": "b241ab0e18eb399d55be",
+    "url": "/static/js/main.9921cf83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
